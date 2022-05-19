f6e1f6fd982a49823fe93dfa9c7a7df7794f3dd5
Lidan-Huang <huangld0314@gmail.com>
2022-05-19 15:32:25 -0700

    fixed continuous adding numbers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState} from 'react';
+import { useState } from 'react';
 import Operators from './Operators';
 import Numbers from './Numbers';
 import Display from './Display';
@@ -9,34 +9,46 @@ function App() {
   const [secondNum, setSecondNum] = useState("");
   const [operator, setOperator] = useState(null);
   const [result, setResult] = useState(0);
+  const [equalSign, setEqualSign] = useState(false);
 
-  function updateResults(){
-    setResult(Number(firstNum) + Number(secondNum));
+  function updateResults() {
+    let newResult = Number(firstNum) + Number(secondNum);
+    setResult(newResult);
+    setEqualSign(true);
     setOperator(null);
-    // setFirstNum("");
-    // setSecondNum("");
+    console.log("result from app:", result);
+    setFirstNum(newResult.toString());
+    console.log("reset first num:", firstNum);
+    setSecondNum("");
   }
 
-  function updateOperator(operation){
-    if(firstNum === "") setFirstNum("0");
+  function updateOperator(operation) {
+    if (firstNum === "") setFirstNum("0");
     setOperator(operation);
+    setEqualSign(false);
   }
 
   function updateNum(num) {
     let newFirstNum = firstNum.concat(num.toString());
     let newSecondNum = secondNum.concat(num.toString());
     if (operator === null) {
-      setFirstNum((+newFirstNum).toString());    
+      setFirstNum((+newFirstNum).toString());
     } else {
       setSecondNum((+newSecondNum).toString());
     }
   }
-  
+
   return (
     <div className="App">
-      <Operators updateOperator={updateOperator} updateResults={updateResults}/>
+      <Operators updateOperator={updateOperator} updateResults={updateResults} />
       <Numbers updateNum={updateNum} />
-      <Display operator={operator} firstNum={firstNum} secondNum={secondNum} result={result}/>
+      <Display
+        operator={operator}
+        firstNum={firstNum}
+        secondNum={secondNum}
+        result={result}
+        equalSign={equalSign}
+      />
     </div>
   );
 }
--- src/Display.js
+++ src/Display.js
@@ -1,13 +1,15 @@
-function Display({ operator, firstNum, secondNum, result }) {
+function Display({ operator, firstNum, secondNum, result, equalSign }) {
   console.log('op', operator);
+  console.log('equal sign:', equalSign);
   console.log('firstNum', firstNum);
   console.log('secondNum', secondNum);
   console.log('result', result);
 
   return (
     <div className="Display">
-      {secondNum === "" && firstNum} {operator !== null && secondNum}
-      {firstNum === "" && secondNum === "" && operator === null && (result)}
+      {secondNum === "" && (!equalSign) && firstNum} 
+      {operator !== null && secondNum}
+      {equalSign && result}
     </div>
   );
 }
